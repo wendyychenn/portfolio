8a9f744a5511f5ec2ccc58d07a79750178e4ec1f
wendyychenn <wchen242@my.bcit.ca>
2024-02-05 10:44:14 -0800

    adjust nav

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,11 +1,15 @@
 import styles from '@/styles/Home.module.css'
 
 export default function NavBar() {
-    return (
-        <div className={`${styles.nav}`}>
-          <a href='/'>Projects</a>
-          <a>Works</a>
-          <a>About</a>
-        </div>
-    )
+  return (
+    <div className={`${styles.nav}`}>
+      <div><h1>Wendy</h1></div>
+      <div className={`${styles.navLinks}`}>
+        <a href='/'>Projects</a>
+        <a>Works</a>
+        <a>About</a>
+      </div>
+    </div>
+
+  )
 }
