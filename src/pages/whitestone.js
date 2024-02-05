91e256ba56475206b2fe80c72a1217537bee3c82
wendyychenn <wchen242@my.bcit.ca>
2024-02-05 10:46:11 -0800

    fix error

--- src/pages/whitestone.js
+++ src/pages/whitestone.js
@@ -5,7 +5,7 @@ import NavBar from '@/components/NavBar'
 export default function Whitestone() {
     return (
         <>
-            <main className={`${styles.main2}`}>
+            <main className={`${styles.main}`}>
 
                 <NavBar />
 
