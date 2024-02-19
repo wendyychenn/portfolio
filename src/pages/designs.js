db385765ec10e7af3378e989088bb2d01e93341e
wendyychenn <wchen242@my.bcit.ca>
2024-02-19 15:01:52 -0800

    design page update

--- src/pages/designs.js
+++ src/pages/designs.js
@@ -14,7 +14,7 @@ export default function Designs() {
                 <h1 className={`${styles.title}`}>Designs and Illustrations</h1>
 
                 <div className={`${styles.layout3}`} style={{display: 'flex', justifyContent: 'center', flexDirection: 'column', gap: '3rem'}}>
-                    <a href='https://youtu.be/-1oNlJvEtDo' target="_blank"><video src='/designs/chocolate.mp4' autoPlay loop muted style={{width: '50vw', borderRadius: '1rem'}}/></a>
+                    <a href='https://youtu.be/-1oNlJvEtDo' target="_blank"><img src='/designs/chocolate.png' style={{width: '50vw', borderRadius: '1rem'}}/></a>
                     <img src='/designs/cat-party.png' style={{ width: '50vw', borderRadius: '1rem' }} />
                     <video src='/designs/car-animation.mp4' autoPlay loop muted style={{width: '50vw', borderRadius: '1rem'}}/>
                     <img src='/designs/amplifier.png' style={{ width: '50vw', borderRadius: '1rem' }} />
